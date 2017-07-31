e4e6a8b48ee248335c43e9a8ad2b6517d7811950
juliebraun <juliebraun07113@gmail.com>
2017-07-31 16:31:33 -0600

    incomplete hmwk

---
+++ constructors.js
@@ -0,0 +1,13 @@
+var basicCardObject = {
+  front: [Question1, Question2, Question3, Question4, Question5],
+  back:  [Answer1, Answer2, Answer3, Answer4 Answer5],
+};
+
+var clozeCardObject = {
+	text: [Text1, Text2, Text3, Text4, Text5],
+	cloze: = [Cloze1, Cloze2, Cloze3, Cloze4, Cloze5]
+
+};
+
+module.exports = CardArray;
+var CardArray = [basicCardObject, clozeCardObject];
